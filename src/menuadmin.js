35543a6d7307993d55a83bbbb0cf0dfe3259d042
anonymousskt <77140403+anonymousskt@users.noreply.github.com>
2021-02-21 20:20:47 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,33 @@
+const menuadmin = (prefix, pushname) => {
+ return `𝐒𝐥𝐯 𝐚𝐪𝐮𝐢 𝐞 𝐨 𝐦𝐞𝐧𝐮 𝐝𝐨𝐬 𝐚𝐝𝐦𝐢𝐧𝐬
+ _𝐨𝐛𝐬 𝐩𝐚𝐫𝐚 𝐨 𝐛𝐨𝐭 𝐞𝐱𝐞𝐜𝐮𝐭𝐚𝐫 𝐞𝐬𝐬𝐞𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐞 𝐧𝐞𝐬𝐜𝐞𝐬𝐬𝐚𝐫𝐢𝐨 𝐝𝐚𝐫 𝐚𝐝𝐦 𝐩𝐚𝐫𝐚 𝐨 𝐛𝐨𝐭_
+ 
+ ◪ *𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐏𝐀𝐑𝐀 𝐀𝐃𝐌𝐈𝐍𝐒*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}remover
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *𝐂𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐝𝐨 𝐁𝐎𝐓*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = paulomenu
